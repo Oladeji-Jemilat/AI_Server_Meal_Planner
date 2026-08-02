17f8dd8db5a828c694e0ac5332a0cb7e03bb36c9
Jameelah <jemeelahtemileyi@gmail.com>
2026-08-02 17:54:47 +0100

    update jwt-secret

--- middlewares/isLoggedIn.js
+++ middlewares/isLoggedIn.js
@@ -79,9 +79,9 @@ const isLoggedIn = async (req, res, next) => {
                 message: "Token is required"
             });
         }
-console.log("JWT_SECRET exists:", !!process.env.JWT_SECRET);
+console.log("JWT_SECRET exists:", !!process.env.jwt_secret);
 console.log("Token exists:", !!token);
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const decoded = jwt.verify(token, process.env.jwt_secret);
         const email = decoded.email;
 
         const isBlacklisted = await blacklistedToken.findOne({ token });
